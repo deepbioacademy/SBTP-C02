83e2addba18627763bef88caaf9684beb891c437
Jubayer Hossain <work.jubayer@gmail.com>
2026-04-03 22:23:48 +0600

    added variables and data types

--- scripts/01_Basics.R
+++ scripts/01_Basics.R
@@ -44,8 +44,28 @@ pak::pkg_install(C("pak1", "pak2", "pak3"))
 pak::pkg_install(c("TCGAbiolinks", "maftools", "DESeq2"))
 
 # Variables 
+# 1. name should be meaningful 
+# 2. use underscore for more than one words (student_height)
+a <- 10
+age <- 10
+my_age <- 30
 
 
+# Data types 
+# Numbers 
+# 1. number of students > count (discrete)
+num_students <- 30 
+class(num_students)
 
+# 2. height of a student > measurement (continuous)
+student_height <- 5.1 
+class(student_height)
 
+# Text/Character 
+# Facebook Post (Sentiment = Negative/Positive)
+# Comment #1: Stupid 
+# Comment #2: This is great. 
+# Comment #3: Good one! 
 
+name <- "Jubayer"
+class(name)
