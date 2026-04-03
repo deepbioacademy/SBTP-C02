f3eb1b9ed02a0f0787b6b95af98e4c648e82584e
Jubayer Hossain <work.jubayer@gmail.com>
2026-04-03 22:06:11 +0600

    added package management

--- scripts/01_Basics.R
+++ scripts/01_Basics.R
@@ -1,1 +1,51 @@
+# R as calculator 
 5 - 2
+2 + 3 
+3/2
+
+# Getting help 
+# 1. Get help of a particular function using "?"
+?mean
+
+# 2. Get help of a particular concept using `help` function
+help(str)
+
+# Install R Packages 
+# 1. CRAN Packages 
+install.packages("package_name")
+install.packages("tidyverse")
+
+# 2. Packages for Bioinformatics (Biconductor)
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.22")
+
+BiocManager::install("package_name")
+BiocManager::install("DESeq2")
+
+# 3. Package Management using `pak`
+# Install pak 
+install.packages("pak")
+
+# Install Single CRAN Package 
+pak::pkg_install("package_name")
+pak::pkg_install("tidyverse")
+
+# Install Multiple CRAN Packages 
+pak::pkg_install(c("pak1", "pak2", "pak3"))
+pak::pkg_install(c("tidyverse", "dplyr", "ggpubr"))
+
+# Install Single Bioconductor Package 
+pak::pkg_install("package_name")
+pak::pkg_install("DESeq2")
+
+# Install Multiple Bioconductor Packages 
+pak::pkg_install(C("pak1", "pak2", "pak3"))
+pak::pkg_install(c("TCGAbiolinks", "maftools", "DESeq2"))
+
+# Variables 
+
+
+
+
+
